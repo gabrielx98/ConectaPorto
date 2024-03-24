400eba949b485e45b7e05c173dbf78065e592725
Gabriel <gabrielxaviercr@hotmail.com>
2024-03-24 00:12:15 -0300

    Construção da arquitetura

---
+++ APIs/C#/ConectaPorto/Services/BizService/Interfaces/IWeatherForecast.cs
@@ -0,0 +1,14 @@
+﻿using ConectaPorto.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConectaPorto.Services.BizService.Interfaces
+{
+    public interface IWeatherForecast
+    {
+        public IEnumerable<WeatherForecast> GerarDados(string[] Summaries);
+    }
+}
