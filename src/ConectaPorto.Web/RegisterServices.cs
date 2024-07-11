ff9b61b212a7906461a1bbb34ff75f3b742be4a9
Gabriel <gabriel.xavier@fsbrcorp.com.br>
2024-07-11 17:36:45 -0300

    injeção de dependencia

---
+++ src/ConectaPorto.Web/RegisterServices.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace ConectaPorto.Web
+{
+    public class RegisterServices
+    {
+        public static void Register(IServiceCollection services)
+        {
+            // Obtém todas as interfaces no assembly do projeto "ConectaPorto"
+            var interfaces = Assembly.Load("ConectaPorto.Web")
+                .GetTypes().AsQueryable().Where(i => i.IsInterface);
+
+            // Para cada interface encontrada, registra a implementação correspondente usando AddScoped
+            foreach (var @interface in interfaces)
+            {
+                var implementation = Assembly.Load("ConectaPorto.Web")
+                    .GetTypes()
+                    .FirstOrDefault(t => @interface.IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+
+                if (implementation != null)
+                {
+                    services.AddScoped(@interface, implementation);
+                }
+            }
+        }
+    }
+}
