d6eedafc82b5f0c646185bfb19ca8edffe0e8b00
Gabriel <gabriel.xavier@fsbrcorp.com.br>
2024-07-11 17:36:19 -0300

    cliente repository

---
+++ src/ConectaPorto.Web/Repository/ClienteRepository.cs
@@ -0,0 +1,27 @@
+﻿using ConectaPorto.Models;
+using RestSharp;
+using ConectaPorto.Web.Repository.Interface;
+
+namespace ConectaPorto.Web.Repository
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly RestClient client;
+        private readonly string origin;
+
+        public ClienteRepository(IConfiguration configuration, IHttpContextAccessor httpContext)
+        {
+            origin = $"{httpContext?.HttpContext?.Request?.Scheme}://{httpContext?.HttpContext?.Request?.Host}";
+            client = new RestClient(configuration["UriApi"]);
+        }
+
+        public async Task<ApiResponse<List<Cliente>>> ObterClientes()
+        {
+            var rest = "/api/Cliente/Listar";
+            var request = new RestRequest(rest, Method.Get);
+            request.AddHeader("Origin",origin);
+            var response = await client.ExecuteAsync(request);
+            return ApiResponse<List<Cliente>>.Create(response);
+        }
+    }
+}
---
+++ src/ConectaPorto.Web/Repository/Interface/IClienteRepository.cs
@@ -0,0 +1,9 @@
+﻿using ConectaPorto.Models;
+
+namespace ConectaPorto.Web.Repository.Interface
+{
+    public interface IClienteRepository
+    {
+        public Task<ApiResponse<List<Cliente>>> ObterClientes();
+    }
+}
