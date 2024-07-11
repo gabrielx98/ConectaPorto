7fd1e3548167e91ed45419351d87b5645941274c
Gabriel <gabriel.xavier@fsbrcorp.com.br>
2024-07-11 17:37:36 -0300

    view, model e extension para listar

---
+++ src/ConectaPorto.Web/Extensions/ListarViewModelExtension.cs
@@ -0,0 +1,38 @@
+﻿using ConectaPorto.Models;
+using ConectaPorto.Web.ViewModel;
+using System.Reflection;
+
+namespace ConectaPorto.Web.Extensions
+{
+    public class ListarViewModelExtension<T>
+    {
+        public static ListarViewModel Converter(List<T> dados) {
+            var retorno = new ListarViewModel();
+            retorno.Linhas = Linhas(dados);
+            retorno.Colunas = Colunas();
+            retorno.TotalItens = dados.Count;
+            retorno.TituloPagina = typeof(T).Name;
+            return retorno;
+        }
+        private static List<string> Colunas()
+        {
+            return typeof(T).GetProperties().Select(x => x.Name).ToList();
+        }
+
+        private static List<List<string>> Linhas(List<T> lista)
+        {
+            var retorno = new List<List<string>>();
+            foreach (var item in lista)
+            {
+                var itemRetorno = new List<string>();
+                PropertyInfo[] properties = typeof(T).GetProperties();
+                foreach (var property in properties)
+                {
+                    itemRetorno.Add(property.GetValue(item).ToString());
+                }
+                retorno.Add(itemRetorno);
+            }
+            return retorno;
+        }
+    }
+}
---
+++ src/ConectaPorto.Web/ViewModel/ListarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ConectaPorto.Models;
+using System.Reflection;
+
+namespace ConectaPorto.Web.ViewModel
+{
+    public class ListarViewModel
+    {
+        public string TituloPagina { get; set; }
+        public List<string> Colunas { get; set; }
+        public List<List<string>> Linhas { get; set; }
+        public int TotalItens { get; set; }
+    }
+}
